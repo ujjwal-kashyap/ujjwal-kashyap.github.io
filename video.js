562b0bfa0bba7afbb433e9e95e317ce454f31574
Ujjwal Kashyap <37990652+ujjwal-kashyap@users.noreply.github.com>
2020-06-16 17:08:00 +1000

    57

--- video.js
+++ video.js
@@ -3,19 +3,24 @@ function UserAction() {
   url ='https://nxul9z35le.execute-api.ap-southeast-2.amazonaws.com/DEV/video';
   axios.get(url,null)
    .then(response => {
-     //console.log(response.data);
      console.log('success');
 
-     // Buffer for the jpg data
-      var buf = response.data;
-      // Create an HTML img tag
-      var imageElem = document.createElement('img');
-      // Just use the toString() method from your buffer instance
-      // to get date as base64 type
-      imageElem.src = 'data:image/jpeg;base64,' + buf;
-      imageElem.height = 500;
-      imageElem.width = 500;
-      document.getElementById('result').appendChild(imageElem);
+     var i;
+     for(i=0; i<response.data.length; i++) {
+       // Buffer for the jpg data
+        var buf = response.data[i];
+        // Create an HTML img tag
+        var imageElem = document.createElement('img');
+        // Just use the toString() method from your buffer instance
+        // to get date as base64 type
+        imageElem.src = 'data:image/jpeg;base64,' + buf;
+        //imageElem.height = 500;
+        imageElem.width = 500;
+        imageElem.border = '2px solid black';
+        document.getElementById('result').appendChild(imageElem);
+     }
+
+
    })
    .catch(error => {
      console.log(error);
