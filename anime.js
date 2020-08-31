679449a7aa5cca598a57ce1fa5f31e165993975e
Ujjwal Kashyap <37990652+ujjwal-kashyap@users.noreply.github.com>
2020-08-31 21:37:08 +1000

    76

---
+++ anime.js
@@ -0,0 +1,110 @@
+function animate(cvs,ctx) {
+
+  //animation area
+  var cW = cvs.width;
+  var cH = cvs.height;
+  const tL1 = "Create Stories";
+  const tL2 = "At";
+  const tL3 = "Lightning Speed";
+  const tL10 = "Visit us";
+  const tL11 = "@AlphaBlue.co";
+  const font = "Electrolize";
+
+  var img = new Image();   // Create new img element
+  //img.crossOrigin = 'anonymous';
+
+
+  /*=============================================================================*/
+  /* Utility Functions
+  /*=============================================================================*/
+  const rand = function(rMi, rMa){return ~~((Math.random()*(rMa-rMi+1))+rMi);};
+
+  /*=============================================================================*/
+  /* BG layer
+  /*=============================================================================*/
+  const refBg = () => {
+    ctx.clearRect(0, 0, cW, cH);
+    ctx.fillStyle = "#152238";
+    ctx.fillRect(0, 0, cW, cH);
+
+    //ctx.globalCompositeOperation = 'destination-out';
+    //ctx.fillStyle = 'rgba(0,0,0,'+rand(1,30)/100+')';
+    //ctx.fillRect(0, 0, cW, cH);
+    //ctx.globalCompositeOperation = 'source-over';
+  };
+
+  /*=============================================================================*/
+  /* Animation Layer
+  /*=============================================================================*/
+  const refBg1 = () => {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, rand(cW/3,cW/2), cH/2);
+
+
+
+
+  };
+
+  /*=============================================================================*/
+  /* Text Layer
+  /*=============================================================================*/
+  const line = (l,h) => {
+    ctx.fillText(l, cW/2, h);
+  }
+
+  // text on bg
+  const text = () => {
+    ctx.font = "70px "+font;
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+
+    line(tL1,cH/4);
+    line(tL2,cH/2.7);
+    line(tL3,cH/2.1);
+
+    //offset=2;
+    ctx.font = "40px "+font;
+    line(tL10,cH/1.5);
+    line(tL11,cH/1.3);
+  }
+
+
+  /*=============================================================================*/
+  /* Loop functions
+  /*=============================================================================*/
+  const animationFunc = () => {
+
+    img.onload = function () {
+
+      const animationFunc1 = () => {
+        //refBg();
+        refBg1();
+        //ctx.drawImage(img, 0, 0);
+        text();
+        window.requestAnimationFrame( animationFunc1 );
+      };
+      animationFunc1();
+
+    };
+    img.src = 'a.JPG'; // Set source path
+
+    //window.requestAnimationFrame( animationFunc );
+  };
+
+  /*=============================================================================*/
+  /* Load font and Init Animation
+  /*=============================================================================*/
+
+  WebFontConfig = {
+   google: { families: [font+':400:latin'] },
+   active: function() {animationFunc();}
+  };
+
+  (function () {
+   var wf = document.createElement("script");
+   wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+   wf.async = 'true';
+   document.head.appendChild(wf);
+  })();
+
+}
---
+++ rec.js
@@ -0,0 +1,71 @@
+window.onload = function() {
+  "use strict";
+
+  let cvs = document.querySelector("canvas");
+  let ctx = cvs.getContext("2d");
+  let vdo = document.querySelector("video");
+  let fps = 30;
+  let vT = 3000;
+
+
+  var videoStream = cvs.captureStream(fps);
+  var mediaRecorder = new MediaRecorder(videoStream);
+
+  var chunks = [];
+  mediaRecorder.ondataavailable = function(e) {
+    console.log({e});
+    chunks.push(e.data);
+  };
+
+  mediaRecorder.onstop = function(e) {
+    var blob = new Blob(chunks, { 'type' : 'video/webm' }); // image/gif    video/mp4
+    console.log(blob);
+    console.log(chunks);
+    chunks = [];
+    var videoURL = URL.createObjectURL(blob);
+    vdo.src = videoURL;
+  };
+
+  //vdo.srcObject = videoStream;
+
+  mediaRecorder.start();
+  animate(cvs,ctx);
+  setTimeout(function (){ mediaRecorder.stop(); }, vT);
+}
+
+
+/*=============================================================================*/
+/* Check Canvas Support
+/*=============================================================================*/
+/*var isCanvasSupported = function(){
+  var elem = document.createElement('canvas');
+  return !!(elem.getContext && elem.getContext('2d'));
+};*/
+
+/*=============================================================================*/
+/* Setup requestAnimationFrame
+/*=============================================================================*/
+/*var setupRAF = function(){
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x){
+    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+    window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+  };
+
+  if(!window.requestAnimationFrame){
+    window.requestAnimationFrame = function(callback, element){
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() { callback(currTime + timeToCall); }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  };
+
+  if (!window.cancelAnimationFrame){
+    window.cancelAnimationFrame = function(id){
+      clearTimeout(id);
+    };
+  };
+};*/
